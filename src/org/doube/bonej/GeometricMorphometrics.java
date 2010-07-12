b3dbbbe8883eb343e745e3fd978228b8634a34c7
Michael Doube <michael@doube.net>
2010-07-12 11:32:28 +0100

    Start geometric morphometrics project

---
+++ src/org/doube/bonej/GeometricMorphometrics.java
@@ -0,0 +1,30 @@
+package org.doube.bonej;
+
+import ij.plugin.PlugIn;
+
+/**
+ * 
+ * Create landmark sets and analyse them using ImageJ's Orthoslice viewer and 3D
+ * Viewer.
+ * 
+ * Targeting:
+ * 	Generation of labelled landmark sets
+ * 	Storing of landmark definitions
+ * 	Storing of landmarks (in TIFF header?)
+ * 	Viewing & editing landmarks synchronised in orthoviewer and 3D viewer
+ * 	Registering stacks based on landmark sets
+ * 	Analysis? PCA?
+ * 	4D viewing of landmark sets from different specimens
+ * 
+ * @author Michael Doube
+ * 
+ */
+public class GeometricMorphometrics implements PlugIn {
+
+	@Override
+	public void run(String arg) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
