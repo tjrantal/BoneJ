3ff521089a1188e585fc1a14a3e5d76cf47a9e8a
Michael Doube <michael@doube.net>
2010-08-16 08:42:14 +0100

    Extend Point3d to avoid duplication of methods

--- src/org/doube/bonej/geomorph/Landmark.java
+++ src/org/doube/bonej/geomorph/Landmark.java
@@ -1,13 +1,23 @@
 package org.doube.bonej.geomorph;
 
+import javax.vecmath.Point3d;
+
 /**
- * Landmark class for storing multidimensional points
+ * Landmark class for storing multidimensional points. Extends Tuple3d / Point3d
+ * by adding a name and timepoint
  * 
  * @author Michael Doube
  * 
  */
-public class Landmark {
-	private double x, y, z;
+public class Landmark extends Point3d {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4713913304554138975L;
+	/**
+	 * UNIX timestamp, the difference, measured in milliseconds, between the
+	 * current time and midnight, January 1, 1970 UTC.
+	 */
 	private long t;
 	private String spaceUnit;
 	private String name;
@@ -19,37 +29,26 @@ public class Landmark {
 		this.name = name;
 	}
 
-	public double getX() {
-		return x;
-	}
-
-	public double getY() {
-		return y;
-	}
-
-	public double getZ() {
-		return z;
-	}
-
 	public long getT() {
 		return t;
 	}
-	
-	public String getName(){
+
+	public String getName() {
 		return name;
 	}
 
-	public void setPos(double x, double y, double z) {
+	public void set(double x, double y, double z, long t) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.t = t;
 	}
 
 	public void setTime(long t) {
 		this.t = t;
 	}
-	
-	public void setName(String name){
+
+	public void setName(String name) {
 		this.name = name;
 	}
 }
