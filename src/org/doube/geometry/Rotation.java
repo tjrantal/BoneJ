b904fdb5a929096bf00747e9c90083de5473524a
Michael Doube <michael@doube.net>
2011-01-26 15:07:33 +0000

    Put methods to do with rotation matrices in their own class, Rotation

---
+++ src/org/doube/geometry/Rotation.java
@@ -0,0 +1,65 @@
+package org.doube.geometry;
+
+import java.util.ArrayList;
+
+public class Rotation {
+
+	private static final double[] NORTH = { 0, 0, 1 };
+
+	/** Generate a list of random rotation matrices */
+	public static ArrayList<double[][]> randomRotations(int nRotations) {
+		double[][] vectors = Vectors.randomVectors(nRotations);
+		ArrayList<double[][]> rotations = new ArrayList<double[][]>(nRotations);
+		for (int i = 0; i < nRotations; i++)
+			rotations.add(vectorToRotation(vectors[i]));
+		return rotations;
+	}
+
+	/**
+	 * Calculate the rotation matrix that rotates the north unit vector [0,0,1]
+	 * to the position of the input vector
+	 * 
+	 * @param vector
+	 * @return
+	 */
+	public static double[][] vectorToRotation(double[] vector) {
+		final double[] axis = Vectors.crossProduct(vector, NORTH);
+		final double angle = Vectors.angle(vector, NORTH);
+		double[][] rotation = axisAngleToRotation(axis, angle);
+		return rotation;
+	}
+
+	/**
+	 * Convert axis-angle to rotation matrix
+	 * 
+	 * @param axis
+	 *            vector of axis
+	 * @param angle
+	 *            angle of rotation
+	 * @see http://www.euclideanspace.com/maths/geometry/rotations/conversions/
+	 *      angleToMatrix/index.htm
+	 * @return
+	 */
+	public static double[][] axisAngleToRotation(double[] axis, double angle) {
+		final double c = Math.cos(angle);
+		final double s = Math.sin(angle);
+		final double t = 1 - c;
+		final double d = Trig.distance3D(axis);
+		final double x = axis[0] / d;
+		final double y = axis[1] / d;
+		final double z = axis[2] / d;
+		double[][] r = new double[3][3];
+
+		r[0][0] = t * x * x + c;
+		r[0][1] = t * x * y - z * s;
+		r[0][2] = t * x * z + y * s;
+		r[1][0] = t * x * y + z * s;
+		r[1][1] = t * y * y + c;
+		r[1][2] = t * y * z - x * s;
+		r[2][0] = t * x * z - y * s;
+		r[2][1] = t * y * z + x * s;
+		r[2][2] = t * z * z + c;
+		return r;
+	}
+
+}
